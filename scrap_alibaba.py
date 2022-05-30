6defaded0af35150ba0eb392c02e9cbc43cc6ad5
Nech07 <72507103+Nech07@users.noreply.github.com>
2022-05-30 16:05:15 +0200

    Countries and link
    Fixed the script for some error for the user input with the link and the possibility to change countries

--- scrap_alibaba.py
+++ scrap_alibaba.py
@@ -7,54 +7,55 @@ from bs4 import BeautifulSoup
 
 
 #Dictionary to change website url to the corresponding url
-COUNTRIES={"italy": "italian",
-          "germany": "german",
-          "france": "french",
-          "netherlands":"dutch",
-          "japan": "japanese",
-          "spain": "spanish", 
-          "china": "chinese",
-          "russia":"russian",
-          "korea": "korean",
-          "indonesia": "indonesian",
-          "thailandia": "thai"}
+COUNTRIES_ALIBABA={"ITALY": "italian",
+          "GERMANY": "german",
+          "FRANCE": "french",
+          "NETHERLANDS":"dutch",
+          "JAPAN": "japanese",
+          "SPAIN": "spanish", 
+          "CHINA": "chinese",
+          "RUSSIA":"russian",
+          "KOREA": "korean",
+          "INDONESIA": "indonesian",
+          "THAILANDIA": "thai"}
 
 #input from user
 def search_alibaba(products_names, products_prices, products_urls, toSearch_product, selected_country, pages):
     
     # Find if the user input is a link or a name
     if "alibaba." in toSearch_product:
-        toSearch_product=toSearch_product.split("/")[4]
-        toSearch_product=toSearch_product.split("_")[0]
-        toSearch_product=toSearch_product.replace("-"," ")
+        response = requests.get(toSearch_product)
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        toSearch_product = soup.find('h1', class_ = 'module-pdp-title').text
 
     #initial contruct of the url
     if selected_country=="china":
         template= "https://chinese.alibaba.com/trade/search?&language=zh&fsb=y&IndexArea=product_en&CatId=&SearchText="
-    elif  selected_country not in COUNTRIES:
+    elif  selected_country not in COUNTRIES_ALIBABA:
         template="https://www.alibaba.com/trade/search?fsb=y&IndexArea=product_en&CatId=&SearchText="
     else:
-        template="https://"+ COUNTRIES[selected_country]+".alibaba.com/trade/search?fsb=y&IndexArea=product_en&CatId=&SearchText="
+        template="https://"+ COUNTRIES_ALIBABA[selected_country]+".alibaba.com/trade/search?fsb=y&IndexArea=product_en&CatId=&SearchText="
     
     toSearch_product=toSearch_product.replace(" ","+")
-
+    products_urls.append(0)
     #construction of url according to pade
     for i in range(pages): 
         page_number="&page="+str(i)+"&f0=y"
         url= template+toSearch_product+page_number
+        print(url)
         #request to url
         html_text=requests.get(url).text
-
-        data = re.search(r"window\.__page__data__config = (\{.*\})", html_text).group(1)
-        data = json.loads(data)
-
-        #appending the elements of the products to the lists
         try:
+            data = re.search(r"window\.__page__data__config = (\{.*\})", html_text).group(1)
+            data = json.loads(data)
+        #appending the elements of the products to the lists
             for offer in data["props"]["offerResultData"]["offerList"]:
                 products_names.append(offer["information"]["puretitle"])
                 price=offer["promotionInfoVO"]["originalPriceTo"]
                 products_prices.append(float(price))
                 products_urls.append(offer["information"]["productUrl"])
-        except KeyError:
+        except:
             print(f"There are only {i+1} pages")
             break
